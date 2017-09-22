d8ea944eddc77885e7f488df39cf37fd5cb48b90
Joe Wemyss <support@javathenutt.com>
2017-09-22 09:49:30 +0100

    removed superflous code in geocoded form

--- src/app/components/GeocodedForm.vue
+++ src/app/components/GeocodedForm.vue
@@ -38,31 +38,6 @@
       </div>
     </form>
     <div v-if="addressTableShown">
-      <!--<md-button class="md-raised md-warn" @click.native="returnToForm">Re-enter address</md-button>
-      <md-table-card>
-        <md-toolbar>
-          <h1 class="md-title">Possible Addresses</h1>
-          <md-button class="md-icon-button">
-            <md-icon>filter_list</md-icon>
-          </md-button>
-        </md-toolbar>
-        <md-table>
-          <md-table-body>
-            <md-table-row v-for="(address, index) in possibleAddresses" :key="index" :md-item="address" v-if="rowShown(index)" @click.native="itemSelected(address)">
-              <md-table-cell :md-numeric="false" ><mdInkRipple ></mdInkRipple>{{address.formatted_address}}</md-table-cell>
-            </md-table-row>
-          </md-table-body>
-        </md-table>
-        <custom-table-pagination
-          :md-size="currentPageSize"
-          md-label="Rows"
-          md-seperator="of"
-          :mdPage="currentTablePage"
-          :mdTotal="possibleAddresses.length"
-          :md-page-options="[5, 10, 25, 50]"
-          @pagination="onPagination"
-        ></custom-table-pagination>
-      </md-table-card>-->
       <select-address-table :possibleAddresses="possibleAddresses" @addressSelectedFromTable="itemSelected" @returnToForm="returnToForm"></select-address-table>
     </div>
     <div v-if="formattedAddressShown">
@@ -83,26 +58,18 @@
   import * as Logger from 'loglevel';
   import {mapGetters} from 'vuex';
   import bus from '../services/bus';
-  import customTablePagination from './CustomTablePagination';
   import {geocodeAddress} from '@/app/services/geocoding';
-  import MdInkRipple from '../../../node_modules/vue-material/src/core/components/mdInkRipple/mdInkRipple.vue';
-  import MdButton from '../../../node_modules/vue-material/src/components/mdButton/mdButton.vue';
   import SelectAddressTable from './SelectAddressTable.vue';
 
 
   export default {
     components: {
-      SelectAddressTable,
-      MdButton,
-      MdInkRipple,
-      customTablePagination},
+      SelectAddressTable
+    },
     name: 'geocoded_form',
 
     data() {
       return {
-        currentTablePage: 1,
-        currentPageSize: 5,
-        selectAddressFromListShown: false,
         showAddressForm: true,
         formattedAddressShown: false,
         sendableAddress: {
@@ -125,10 +92,6 @@
         possibleAddresses: [],
         googleFormattedAddress: '',
         addressTableShown: false,
-        tableViewBound:{
-          upper: 5,
-          lower: 0
-        }
       }
     },
     computed: {
@@ -143,11 +106,6 @@
           return false;
         }
         return true;
-      },
-      tableBounds(){
-        Logger.info(`testing the current view bounds for table`);
-        Logger.info(`any item with index between ${((this.currentPageSize * this.currentTablePage)) - this.currentPageSize} and ${(this.currentPageSize * this.currentTablePage) -1} should be shown`);
-        return {upper: (this.currentPageSize * this.currentTablePage) -1, lower: ((this.currentPageSize * this.currentTablePage)) - this.currentPageSize};
       }
     },
     methods: {
@@ -162,17 +120,6 @@
         this.chosenAddress.googleDetails = item;
         this.googleFormattedAddress      = item.formatted_address;
         this.formattedAddressShown       = true;
-      },
-      rowShown(index){
-        Logger.info(`attempting to see if row with index ${index} should be shown`);
-        Logger.info(`row shown: ${index >= this.tableBounds.lower && index <= this.tableBounds.upper}`);
-        return index >= this.tableBounds.lower && index <= this.tableBounds.upper
-      },
-      onPagination(data){
-        Logger.info(`table pagination event recieved`);
-        Logger.info(`data recieved: ${JSON.stringify(data)}`);
-        this.currentTablePage = data.page;
-        this.currentPageSize = data.size;
       },
       async checkAddress() {
         Logger.info(`check address button clicked`);
@@ -245,7 +192,6 @@
         this.formattedAddressShown = false;
         bus.$emit('showSnack', 'please adjust your search parameters and try again')
       }
-
     }
   }
 </script>
